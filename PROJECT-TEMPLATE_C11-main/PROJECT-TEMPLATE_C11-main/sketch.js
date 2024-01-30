fb33e9d5e47d23153f8c724311f5190534b51683
starsatar <156380663+starsatar@users.noreply.github.com>
2024-01-30 13:22:50 -0300

    Add files via upload

---
+++ PROJECT-TEMPLATE_C11-main/PROJECT-TEMPLATE_C11-main/sketch.js
@@ -0,0 +1,18 @@
+
+function preload(){
+shipimg1 = loadanimation(ship-1)(ship-2)(ship-3)(ship-4);
+seaimg = loadimage("sea.png")
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+    drawSprites();
+  
+ 
+}
